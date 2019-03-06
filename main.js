9dfd78b1fba7e3485d677bec6cbc9d395072a41e
Dalibor <info@skydream.info>
2019-03-06 11:47:04 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+let attempt = 3; //Count number of attempts up to 3
+
+// executes on click of login button
+function validate(){
+	let username = document.getElementById("username").value; //username value
+	let password = document.getElementById("password").value; //password value
+
+	if ( username == "skydream" && password == "skydreamweb"){ //check if username and password match
+		alert ("Login successfully");
+		window.location = "success.html"; //redirecting to other page
+		return false;
+	}
+	else{
+		attempt --; // decrementing by one for every tried login
+		alert("You have left "+attempt+" attempt!");
+
+		// disabling fields after 3 attempts
+		if( attempt == 0){
+			document.getElementById("username").disabled = true;
+			document.getElementById("password").disabled = true;
+			document.getElementById("submit").disabled = true;
+			return false;
+		}
+	}
+}
